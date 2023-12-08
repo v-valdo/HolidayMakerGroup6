fb56f129ae60009dab7fbd7730b3346d9991b29c
Karl Mårtensson <karlmartenssonsales@gmail.com>
2023-12-08 10:35:27 +0100

    Register Customer

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HolidayMakerGroup6;
+
+Customers customer = new Customers();
+customer.RegisterCustomer();
+
+
