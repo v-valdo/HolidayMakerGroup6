60a1d60ce01a4bacb9fe35ae77a2dbcceea0deb7
Karl Mårtensson <karlmartenssonsales@gmail.com>
2023-12-15 18:12:39 +0100

    list items DONE

--- Booking.cs
+++ Booking.cs
@@ -1,4 +1,5 @@
 ﻿using Npgsql;
+using System.Runtime.InteropServices;
 
 namespace HolidayMakerGroup6;
 public class Booking
@@ -8,15 +9,6 @@ public class Booking
 	public Location? Location;
 	public Extras Extra;
 	public double Total;
-
-	public Booking(Customer? customer, Room? room, Location? location, Extras extra, double total)
-	{
-		Customer = customer;
-		Room = room;
-		Location = location;
-		Extra = extra;
-		Total = total;
-	}
 
 	public async Task Add()
 	{
@@ -36,18 +28,42 @@ public class Booking
 
 	public async Task Edit()
 	{
-		// Visa alla bookings -> Välj booking att edita
-		List();
-	}
+        // Visa alla bookings -> Välj booking att edita
 
-	// callable Method summing the total price (incl. extras) for the booking
-	public void CalculatePrice()
+    }
+
+    // callable Method summing the total price (incl. extras) for the booking
+    public void CalculatePrice()
 	{
 		// typ Room.Price + Extras.Price
 	}
 
-	public void List()
-	{
-        
+    public async Task List()
+    {
+        await using var connection = NpgsqlDataSource.Create(Database.Url);
+
+        await connection.OpenConnectionAsync();
+
+        using var cmd = connection.CreateCommand("SELECT CONCAT(c.first_name, ' ', c.last_name) AS Customer, b.customer_id AS CustomerID, b.id AS BookingID FROM BOOKINGS b JOIN Customers c ON b.customer_id = c.id;");
+
+        using var reader = await cmd.ExecuteReaderAsync();
+
+        Console.WriteLine("List of Bookings:");
+
+        int count = 1;
+
+        while (await reader.ReadAsync())
+        {
+            string customerName = reader.GetString(reader.GetOrdinal("Customer"));
+            int customerId = reader.GetInt32(reader.GetOrdinal("CustomerID"));
+            int bookingId = reader.GetInt32(reader.GetOrdinal("BookingID"));
+
+            Console.WriteLine($"{count}. {customerName}, Customer ID: {customerId}, Booking ID: {bookingId}");
+            count++;
+        }
+
     }
 }
+
+
+
