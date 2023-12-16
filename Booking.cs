5013ef8ab29f4475368a4630c49d7a6d7eca7893
Karl Mårtensson <karlmartenssonsales@gmail.com>
2023-12-16 12:53:40 +0100

    List klar

--- Booking.cs
+++ Booking.cs
@@ -28,8 +28,7 @@ public class Booking
 
 	public async Task Edit()
 	{
-        // Visa alla bookings -> Välj booking att edita
-
+        List();
     }
 
     // callable Method summing the total price (incl. extras) for the booking
@@ -44,7 +43,7 @@ public class Booking
 
         await connection.OpenConnectionAsync();
 
-        using var cmd = connection.CreateCommand("SELECT CONCAT(c.first_name, ' ', c.last_name) AS Customer, b.customer_id AS CustomerID, b.id AS BookingID FROM BOOKINGS b JOIN Customers c ON b.customer_id = c.id;");
+        using var cmd = connection.CreateCommand("SELECT CONCAT(c.first_name, ' ', c.last_name) AS Customer, b.customer_id AS CustomerID, b.id AS BookingID, (b.start_date || ' - ' || b.end_date) AS StartdateEndDate FROM BOOKINGS b JOIN Customers c ON b.customer_id = c.id;");
 
         using var reader = await cmd.ExecuteReaderAsync();
 
@@ -57,13 +56,15 @@ public class Booking
             string customerName = reader.GetString(reader.GetOrdinal("Customer"));
             int customerId = reader.GetInt32(reader.GetOrdinal("CustomerID"));
             int bookingId = reader.GetInt32(reader.GetOrdinal("BookingID"));
+            string startdateEndDate = reader.GetString(reader.GetOrdinal("StartdateEndDate"));
 
-            Console.WriteLine($"{count}. {customerName}, Customer ID: {customerId}, Booking ID: {bookingId}");
+
+            Console.WriteLine($"{count}. {customerName}, Customer ID: {customerId}, Booking ID: {bookingId}, Date: {startdateEndDate}");
             count++;
         }
-
     }
 }
+
 
 
 
