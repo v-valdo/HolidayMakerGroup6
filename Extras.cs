97036314e50a47736ae3e889aa088aba5b9231ca
matilda002 <matilda0817@hotmail.com>
2023-12-19 11:40:14 +0100

    now shows all extras when adding extra to booking

--- Extras.cs
+++ Extras.cs
@@ -13,19 +13,22 @@ public class Extras
     public async Task <string> ShowAll()
     {
         string result = string.Empty;
-        const string query = @"select name, price from extra_service;";
+        const string query = @"select id, name, price from extra_service;";
 
-        await Console.Out.WriteLineAsync("Service              || Price               \n" +
-                                         "---------------------||-----------------------");
+        await Console.Out.WriteLineAsync("ID     || Service             || Price               \n" +
+                                         "-------||---------------------||-------------------");
         var reader = await _db.CreateCommand(query).ExecuteReaderAsync();
         while (await reader.ReadAsync())
         {
-            string service = reader.GetString(0);
-            string price = reader.GetDecimal(1).ToString();
+            string id = reader.GetInt32(0).ToString();
+            string service = reader.GetString(1);
+            string price = reader.GetDecimal(2).ToString();
 
-            result += " " + reader.GetString(0) + new string(' ', 20 - service.Length);
+            result += " " + reader.GetInt32(0) + new string(' ', 6 - id.Length);
             result += "||";
-            result += " " + reader.GetDecimal(1) + " kr" + new string(' ', 10 - price.Length);
+            result += " " + reader.GetString(1) + new string(' ', 20 - service.Length);
+            result += "||";
+            result += " " + reader.GetDecimal(2) + " kr" + new string(' ', 10 - price.Length);
             result += "\n";
         }
         return result;
@@ -37,9 +40,11 @@ public class Extras
         try
         {
             Console.Clear();
-            Console.WriteLine("Booking id: ");
+            await Console.Out.WriteAsync(await ShowAll());
+            await Console.Out.WriteLineAsync("---------------------------------------------------");
+            await Console.Out.WriteAsync("Insert booking id: ");
             var bookingidinput = Console.ReadLine();
-            Console.Write("Extras id: ");
+            await Console.Out.WriteAsync("Insert extras id: ");
             var extraserviceidinput = Console.ReadLine();
 
             Console.Clear();
